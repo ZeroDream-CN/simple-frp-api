4819bbe8c5601daee08f46fd258118dc8dc2da4b
Akkariin Meiko <akkariins@gmail.com>
2019-08-21 15:43:34 +0800

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,12 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Records of proxies
+-- ----------------------------
+INSERT INTO `proxies` VALUES ('1', 'Akkariin', 'Test', 'tcp', null, null, null, null, null, null, null, null, null, '13145', null, null, null, '0', null);
+INSERT INTO `proxies` VALUES ('2', 'Akkariin', 'Test2', 'http', null, null, '[\"test.com\"]', null, null, null, null, null, null, '', null, null, null, '0', null);
+
+-- ----------------------------
+-- Records of tokens
+-- ----------------------------
+INSERT INTO `tokens` VALUES ('1', 'Akkariin', 'usertoken123456', '0');
